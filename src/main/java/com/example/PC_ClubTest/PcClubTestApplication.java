cf8b28eff8769b1225c78db9a87e3460bfd75f2a
Cobraboom <lomackin.vlad@yandex.ru>
2021-12-21 20:37:16 +0300

    First initial project

---
+++ src/main/java/com/example/PC_ClubTest/PcClubTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.PC_ClubTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcClubTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PcClubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PC_ClubTest/PcClubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PC_ClubTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcClubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
